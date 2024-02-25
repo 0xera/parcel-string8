2dd49d213836121f1f5ef753c294e6c45aa88d24
0xera <56160164+0xera@users.noreply.github.com>
2024-02-26 00:20:06 +0300

    add some comparison results

--- benchmark/src/main/kotlin/com/example/benchmark/generators.kt
+++ benchmark/src/main/kotlin/com/example/benchmark/generators.kt
@@ -2,5 +2,5 @@ package com.example.benchmark
 
 import kotlin.random.Random
 
-fun generateStringsList(count: Int) = (0 until count).map { "привет" } // "hello"
+fun generateStringsList(count: Int) = (0 until count).map { "hello" } // "hello", "hello$it", "привет", "привет$it", "你好", "你好$it"
 fun generateLongsList(count: Int) = (0 until count).map { Random.nextLong() }
